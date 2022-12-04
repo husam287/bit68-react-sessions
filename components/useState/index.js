170f3fa89a67eca98d1cdae1cfb684af0d3f3335
husam287 <husam287@hotmai.com>
2022-12-04 18:32:56 +0200

    add usestate session

---
+++ components/useState/index.js
@@ -0,0 +1,22 @@
+import Text from 'components/General/Text'
+import React, { useState } from 'react'
+
+export const UseStateComponent = () => {
+  const [count, setCount] = useState(0)
+
+  console.log('#### UseState Render ####')
+  return (
+    <div className='text-dark d-flex justify-content-center flex-column align-items-center mt-5'>
+      <div>
+        <button onClick={() => setCount(c => c + 1)}>Count +1</button>
+        <button onClick={() => setCount(0)}>Count to 0</button>
+        <button onClick={() => setCount(5)}>Count to 5</button>
+      </div>
+      <div>
+        <Text isTranslated={false}>
+          {`Count : ${count}`}
+        </Text>
+      </div>
+    </div>
+  )
+}
